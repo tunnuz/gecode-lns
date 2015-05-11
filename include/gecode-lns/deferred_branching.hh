2185fb545c3ea1ccd2ed45c33c8c30de49e5e6e5
Tommaso Urli <tunnuz@gmail.com>
2015-05-11 16:17:17 +1000

    Removed useless function.

--- include/gecode-lns/deferred_branching.hh
+++ include/gecode-lns/deferred_branching.hh
@@ -6,17 +6,17 @@
 using namespace Gecode;
 
 /**
- A class template representing a Space whose branching is posted separately from the model. 
+ A class template representing a Space whose branching is posted separately from the model.
  The template class (T) should redefine the basic branching styles.
  @remarks the cost variable is "cost".
  */
 class DeferredBranchingSpace
 {
 public:
-  
+
   /** Post a branching good for performing a typical tree search, e.g., B&B, DFS, ... */
   virtual void tree_search_branching() {  }
-  
+
 };
 
 /**
@@ -26,7 +26,7 @@ template <class T, class O>
 class InstantBranchingSpace : public T
 {
 public:
-  
+
   // Gecode default constructor
   InstantBranchingSpace<T, O>(const O& o) : T(o)
   {
@@ -36,7 +36,7 @@ public:
   /** Gecode copy constructor (branching already posted) */
   InstantBranchingSpace<T, O>(bool share, InstantBranchingSpace<T, O>& t) : T(share, t)
   { }
-  
+
   /** Gecode copy method */
   virtual Space* copy(bool share)
   {
--- include/gecode-lns/lns_space.hh
+++ include/gecode-lns/lns_space.hh
@@ -9,16 +9,19 @@ using namespace Gecode;
 class LNSAbstractSpace
 {
 public:
+
   /** Post a random branching, e.g. good for finding a random initial solution in LNS */
   virtual void initial_solution_branching(unsigned long int restart) = 0;
+
   /** Post a branching for LNS iteration step, the idea is that it should likely find a good solution  */
   virtual void neighborhood_branching() = 0;
+
   /** Method to generate a relaxed solution (i.e., a neighbor) from the current one (this) */
   virtual unsigned int relax(Space* neighbor, unsigned int free) = 0;
-  /** Returns the number of relaxable variables */
-  virtual unsigned int relaxable_vars() const = 0;
+
   /* Returns whether the current space is improving w.r.t. s */
   virtual bool improving(const Space& s, bool strict = true) = 0;
+
   /* Constrain current solution cost to improve over the one passed as parameter plus/minus a delta */
   virtual void constrain(const Space& s, bool strict, double delta) = 0;
 };
@@ -35,20 +38,19 @@ public:
     else
       return this->cost().val() <= _s.cost().val();
   }
-  
+
   virtual void constrain(const Space& s, bool strict, double delta)
   {
     const LNSScript& _s = dynamic_cast<const LNSScript&>(s);
     if (strict)
       rel(*this, this->cost() < _s.cost().val() + delta);
     else
-      rel(*this, this->cost() <= _s.cost().val() + delta);      
+      rel(*this, this->cost() <= _s.cost().val() + delta);
   }
-  
+
 protected:
   LNSScript() : ScriptType(nullptr) {}
   LNSScript(bool share, LNSScript& s) : ScriptType(share,s) {}
 };
 
 #endif
-
